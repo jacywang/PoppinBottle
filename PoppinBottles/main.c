da6c9bfef16398cfb77fe00541313591d1534d12
Jacy Wang <jacywj@gmail.com>
2015-05-06 00:13:41 -0700

    Finish Task 1

--- PoppinBottles/main.c
+++ PoppinBottles/main.c
@@ -8,8 +8,39 @@
 
 #include <stdio.h>
 
+int bottlesAfterRecyling(int totalRecycledPops, int bottlesSum, int capsSum);
+int const costOfOneBottleCop = 2;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int moneyPaid = 10;
+    int boughtPops = moneyPaid / costOfOneBottleCop;
+    int totalRecycledPops = 0;
+    int bottlesSum = boughtPops;
+    int capsSum = boughtPops;
+    totalRecycledPops = bottlesAfterRecyling(totalRecycledPops, bottlesSum, capsSum);
+    printf("Total recycled bottles: %d\n", totalRecycledPops);
     return 0;
+}
+
+int bottlesAfterRecyling(int totalRecycledPops, int bottlesSum, int capsSum) {
+    int numberRecyledByBottles = bottlesSum / 2;
+    int numberRecyledByCaps = capsSum / 4;
+    
+    int numberOfBottlesLeft = bottlesSum % 2;
+    int numberOfCapsLeft = capsSum % 4;
+    
+    int bottlesRecyled = numberRecyledByBottles + numberRecyledByCaps;
+    
+    bottlesSum = numberOfBottlesLeft + bottlesRecyled;
+    capsSum = numberOfCapsLeft + bottlesRecyled;
+    totalRecycledPops += bottlesRecyled;
+    printf("totalRecycles - %d\n", totalRecycledPops);
+    printf("bottlesSum - %d\n", bottlesSum);
+    printf("capsSum - %d\n", capsSum);
+    printf("------\n");
+    if (bottlesSum < 2 && capsSum < 4) {
+        return totalRecycledPops;
+    } else {
+        return bottlesAfterRecyling(totalRecycledPops, bottlesSum, capsSum);
+    }
 }
