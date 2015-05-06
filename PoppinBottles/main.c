34750971df4b42eafc28f43619f83a7091d04747
Jacy Wang <jacywj@gmail.com>
2015-05-05 20:38:41 -0700

    Initialize the repo

---
+++ PoppinBottles/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PoppinBottles
+//
+//  Created by JIAN WANG on 5/5/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
